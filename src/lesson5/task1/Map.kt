02504d9b73e8df119c3b18ef1d890af3282d4e93
grisc <grishadekelman@gmail.com>
2020-11-23 15:39:53 +0300

    blm

--- src/lesson5/task1/Map.kt
+++ src/lesson5/task1/Map.kt
@@ -127,9 +127,8 @@ fun containsIn(a: Map<String, String>, b: Map<String, String>): Boolean = TODO()
  *   subtractOf(a = mutableMapOf("a" to "z"), mapOf("a" to "z"))
  *     -> a changes to mutableMapOf() aka becomes empty
  */
-fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>) {
-    TODO()
-}
+fun subtractOf(a: MutableMap<String, String>, b: Map<String, String>) = TODO()
+
 
 /**
  * Простая (2 балла)
